36cf6a18f39a39bd14500de6183c4a8a0f637480
stokedcheetah <70112751+stokedcheetah@users.noreply.github.com>
2021-09-01 08:08:45 -0400

    Create AgeOnPlanets.cs

---
+++ AgeOnPlanets.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace PlanetCalculations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {  
+      // Your Age
+        Console.WriteLine("How old are you?");
+        int userAge = Console.ReadLine();
+
+      // Length of years on Jupiter (in Earth years)
+      ageJupiter = userAge / 11.86;
+
+      // Age on Jupiter
+
+
+      // Time to Jupiter
+
+
+      // New Age on Earth
+
+
+      // New Age on Jupiter
+
+
+      // Log calculations to console
+
+
+
+    }
+  }
+}
